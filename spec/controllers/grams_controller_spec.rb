cc1f8f82d27a3f8fdbb62d5812bfe4877927534b
Raquele Crotti <raquelecrotti@gmail.com>
2019-10-15 07:24:22 +0000

    added model and view to the grams controller

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -10,7 +10,19 @@ RSpec.describe GramsController, type: :controller do
 
   describe "grams#new action" do
       it "should successfully show the new form" do
-        get :new
+        get :new #trigger an HTTP GET request to the new action 
+        expect(response).to have_http_status(:success) #we expect the response to have an HTTP status code that's successful
       end
   end
+
+  describe "grams#create action" do
+    it "should successfully create a new gram in our database" do
+      post :create, params: { gram: {message: 'Hello!' } } #HTTP post request to the create action-user needs to fill out form with 'Hello!'
+      expect(response).to redirect_to root_path #Expect the response to redirect to root path
+    
+      gram = Gram.last #load the last gram created in our database
+      expect(gram.message).to eq("Hello!") #verifies the gram equals "Hello!"
+    end
+  end
+
 end
