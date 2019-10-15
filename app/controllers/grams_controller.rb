cf1d2793a6cc3d07bcc0a7953b0cdd1ee26819d7
Raquele Crotti <raquelecrotti@gmail.com>
2019-10-15 07:33:01 +0000

    Set up the model and database migration and form to upload a gram onto the app. Used TDD in the spec/controllers/grams_controller_spec.rb file and tested in terminal bundle exec rspec with no failures

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -5,4 +5,15 @@ class GramsController < ApplicationController
 
   def index
   end
+
+  def create
+    @gram = Gram.create(gram_params) #save the gram to our database
+    redirect_to root_path
+  end
+
+  private
+
+  def gram_params
+    params.require(:gram).permit(:message) #pull submitted form data from the params
+  end
 end
