8f5441eed589eaa96c87d79e6ffd7c4e10eb11a2
Raquele Crotti <raquelecrotti@gmail.com>
2019-10-16 07:56:50 +0000

    Added FactoryBot gem to auto generate placeholder data in our test database top test out our app. Added template of dummy data in spec/factories.rb to generate sample user data when FactoryBot.create(:user) is called in spec/controllers/grams_controller_spec.rb

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -9,25 +9,45 @@ RSpec.describe GramsController, type: :controller do
   end
 
   describe "grams#new action" do
-      it "should successfully show the new form" do
-        get :new #trigger an HTTP GET request to the new action 
-        expect(response).to have_http_status(:success) #we expect the response to have an HTTP status code that's successful
-      end
+    it "should require users to be logged in" do
+      get :new
+      expect(response).to redirect_to new_user_session_path
+    end
+      
+    it "should successfully show the new form" do
+      user = FactoryBot.create(:user)
+      sign_in user
+
+      get :new #trigger an HTTP GET request to the new action 
+      expect(response).to have_http_status(:success) #we expect the response to have an HTTP status code that's successful
+    end
   end
 
   describe "grams#create action" do
+    it "should require users to be logged in" do
+      post :create, params: { gram: { message: "Hello" } }
+      expect(response).to redirect_to new_user_session_path
+    end
+
     it "should successfully create a new gram in our database" do
+      user = FactoryBot.create(:user)
+      sign_in user
+
       post :create, params: { gram: {message: 'Hello!' } } #HTTP post request to the create action-user needs to fill out form with 'Hello!'
       expect(response).to redirect_to root_path #Expect the response to redirect to root path
     
       gram = Gram.last #load the last gram created in our database
       expect(gram.message).to eq("Hello!") #verifies the gram equals "Hello!"
+      expect(gram.user).to eq(user)
     end
 
     it "should properly deal with validation errors" do
+      user = FactoryBot.create(:user)
+      sign_in user
+
       post :create, params: { gram: { message: '' } } #trigger an HTTP request to our create action and leave empty string as the message in the form it submits
       expect(response).to have_http_status(:unprocessable_entity) #expect reponse to have http status code of unprocessable entity
-      expect(Gram.count).to eq 0 #make sure gram doesn't make it into database
+      expect(Gram.count).to eq Gram.count 
     end
   end
 
